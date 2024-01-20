2bef411fc16e58508d6359a851f78c8bc9d37425
Z0F <28806618+0xZ0F@users.noreply.github.com>
2024-01-20 13:52:49 -0500

    add iocp lib

--- Capstone/Server/Server.cpp
+++ Capstone/Server/Server.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 
-#include "Common/DebugPrint.h"
+#include "../Common/DebugPrint.h"
+#include "../IOCP/ServerLib/IOCP.hpp"
 
+bool ProcessPacketCb(IOCP::IOCPContext& context)
+{
+	std::string data{};
+	context.GetBufferContents(data);
+
+	DEBUG_PRINT("ProcessPacketCb(): %s\n", data.c_str());
+	return true;
+}
+
+bool MoreDataCb(const std::string_view& recvd, size_t& amountLeft)
+{
+	return false;
+}
 
 int main(int argc, char** argv)
 {
 	DEBUG_PRINT("Debug On\n");
+
+	IOCP::IOCP iocp{};
+	if(!iocp.Begin(9000))
+	{
+		return -1;
+	}
+
+	std::getchar();
 
 	return 0;
 }
